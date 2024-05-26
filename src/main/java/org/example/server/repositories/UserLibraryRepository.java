9f8f89c677f8931f95140e22accae6bc329fbe09
atrukhan <sashatruhan2003@gmail.com>
2024-05-26 14:29:22 +0300

    server v3 fix

--- src/main/java/org/example/server/repositories/UserLibraryRepository.java
+++ src/main/java/org/example/server/repositories/UserLibraryRepository.java
@@ -5,10 +5,12 @@ import org.example.server.models.UserLibrary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserLibraryRepository extends JpaRepository<UserLibrary, Long> {
     Optional<UserLibrary> findById(Long id);
+    List<UserLibrary> findAllById(Iterable<Long> id);
     Optional<UserLibrary> findByCode(String code);
 }
